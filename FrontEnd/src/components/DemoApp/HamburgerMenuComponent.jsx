1e85d18638f52d5a8bdf8faecc41d1e7993fbba2
hcsin <54602584+hcsin@users.noreply.github.com>
2019-12-19 11:54:53 +0100

    updata
    hamburger menu

--- FrontEnd/src/components/DemoApp/HamburgerMenuComponent.jsx
+++ FrontEnd/src/components/DemoApp/HamburgerMenuComponent.jsx
@@ -5,6 +5,7 @@ import hexagonlogo from '../images/hexagonlogo_A1_Rectangle_13_pattern.png'
 import menu_icon from '../images/Menu button.png'
 import menu_icon_exit from '../images/Menu button exit.png'
 import '../../HamburgerMenu.css'
+import {USERROLE} from "./WelcomeComponent";
 
 class HamburgerMenuComponent extends Component{
 
@@ -41,21 +42,30 @@ class HamburgerMenuComponent extends Component{
     }
 
     viewMenu(){
+        const isAdmin = sessionStorage.getItem(USERROLE) === "ROLE_ADMIN" ? true : false;
         const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
         const {isHamburgerMenuOpen} = this.state;
         return (            
             <nav className="m-menu-bar">                    
                 <ul className="m-navigation">
                     {isUserLoggedIn && <li><Link className="m-menu-icon" to="/welcome" onClick={()=> this.closeMenu(isHamburgerMenuOpen)}>HOME</Link></li>}
-                    {isUserLoggedIn && <li><Link className="m-menu-icon" to="/profile" onClick={()=> this.closeMenu(isHamburgerMenuOpen)}>MY PROFILE</Link></li>}
-                    {isUserLoggedIn && <li><Link className="m-menu-icon" to="/demos" onClick={()=> this.closeMenu(isHamburgerMenuOpen)}>DEMO'S</Link></li>}
+                    {isAdmin ? (
+                        <li>{isUserLoggedIn && <Link className="m-menu-icon" to="/members" onClick={()=> this.closeMenu(isHamburgerMenuOpen)}>MEMBERS</Link>}</li>
+                    ) : (
+                        <li>{isUserLoggedIn && <Link className="m-menu-icon" to="/profile" onClick={()=> this.closeMenu(isHamburgerMenuOpen)}>MY PROFILE</Link>}</li>
+                    )}
+                    {isAdmin ? (
+                        <li>{isUserLoggedIn && <Link className="m-menu-icon" to="/demosadmin" onClick={()=> this.closeMenu(isHamburgerMenuOpen)}>DEMO'S</Link>}</li>
+                    ) : (
+                        <li>{isUserLoggedIn && <Link className="m-menu-icon" to="/demosuser" onClick={()=> this.closeMenu(isHamburgerMenuOpen)}>DEMO'S</Link>}</li>
+                    )}
                     {isUserLoggedIn && <li><Link className="m-menu-icon" to="/logout" onClick={()=> this.logoutClicked(isHamburgerMenuOpen)}>LOG OUT</Link></li>}
                 </ul>
             </nav>        
         )    
     }
 
-    render(){          
+    render(){
         const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
         const {isHamburgerMenuOpen} = this.state;
         let mobileMenu;
